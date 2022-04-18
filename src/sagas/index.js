fd7b15e48fe9174d1f8aba8393089fabaa3cf94a
RebikHub <76577962+RebikHub@users.noreply.github.com>
2022-04-18 14:35:27 +0700

    Update index.js

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -8,23 +8,6 @@ import {
 } from '../store/slices';
 import {spawn, put, take, call, fork} from 'redux-saga/effects';
 
-// export const getServicesEpic = (action$) => action$.pipe(
-//   ofType(FETCH_GET_REQUEST),
-//   concatMap(() => ajax.getJSON(process.env.REACT_APP_API_URL).pipe(
-//     map((o) => fetchGetSuccess(o)),
-//     catchError((e) => of(fetchGetError(e))),
-//   ))
-// )
-
-// export const getIdServicesEpic = (action$) => action$.pipe(
-//   ofType(FETCH_GET_ID_REQUEST),
-//   map((o) => o.payload.id),
-//   concatMap((id) => ajax.getJSON(`${process.env.REACT_APP_API_URL}/${id}`).pipe(
-//     map((o) => fetchGetIdSuccess(o)),
-//     catchError((e) => of(fetchGetIdError(e))),
-//   ))
-// )
-
 async function getServices() {
   const response = await fetch(process.env.REACT_APP_API_URL);
   if (!response.ok) {
